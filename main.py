a0c2ac62c54133b333be59d271e283f4ebe792ca
aliprf <aliprf@gmail.com>
2020-05-28 10:32:06 -0600

    creating wflw data

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ if __name__ == '__main__':
 
     '''augment, normalize, and save pts'''
 
-    tf_record_util.rotaate_and_save(dataset_name=DatasetName.wflw)
-    tf_record_util.random_augment_from_rotated(dataset_name=DatasetName.cofw)
+    # tf_record_util.rotaate_and_save(dataset_name=DatasetName.wflw)
+    tf_record_util.random_augment_from_rotated(dataset_name=DatasetName.wflw)
 
     '''normalize the points and save'''
-    tf_record_util.normalize_points_and_save(dataset_name=DatasetName.cofw)
+    tf_record_util.normalize_points_and_save(dataset_name=DatasetName.wflw)
     '''generate pose using hopeNet'''
-    tf_record_util.detect_pose_and_save(dataset_name=DatasetName.cofw)
+    tf_record_util.detect_pose_and_save(dataset_name=DatasetName.wflw)
     '''create tfRecord:'''
     # tf_record_util.create_tf_record(dataset_name=DatasetName.ibug, dataset_type=None, heatmap=False)
     '''retrive and test tfRecords'''
