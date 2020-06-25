94bcc7e6ba3f9fad5792527128aae3e910eb2baf
aliprf <aliprf@gmail.com>
2020-06-25 14:58:50 -0600

    calculating FLOPs added to the cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -44,6 +44,21 @@ class CNNModel:
 
         return model
 
+    def calculate_flops(self, _net):
+        run_meta = tf.RunMetadata()
+        with tf.Session(graph=tf.Graph()) as sess:
+            K.set_session(sess)
+            # net = mobilenet_v2.MobileNetV2(alpha=.75, input_tensor=tf.placeholder('float32', shape=(1, 32, 32, 3)))
+            net = self.create_ASMNet(inp_tensor=None, inp_shape=[224,224,3], output_len=136)
+            opts = tf.profiler.ProfileOptionBuilder.float_operation()
+            flops = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
+
+            opts = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()
+            params = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
+
+            print("{:,} --- {:,}".format(flops.total_float_ops, params.total_parameters))
+            return flops.total_float_ops, params.total_parameters
+
     def create_MobileNet(self, inp_tensor, output_len):
         mobilenet_model = mobilenet_v2.MobileNetV2(input_shape=None,
                                                    alpha=1.0,
