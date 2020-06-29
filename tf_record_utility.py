be323578752d9af8010d8816c0e79ae3156440ab
aliprf <aliprf@gmail.com>
2020-06-29 12:17:48 -0600

    creating map

--- tf_record_utility.py
+++ tf_record_utility.py
@@ -1365,6 +1365,8 @@ class TFRecordUtility:
                 '''load landmark npy, (has been augmented already)'''
                 landmark_file_name = os.path.join(landmarks_dir, img_file_name[:-3] + "npy")
                 landmark = load(landmark_file_name)
+                landmark = str(landmark)
+                print(landmark)
                 landmark_key = self.get_hash_key(landmark)
                 map[landmark_key] = img_file_name
 
