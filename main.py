2b99329945bf28a5c52eef18a9ff9eb305ec0e26
aliprf <aliprf@gmail.com>
2020-07-08 11:59:40 -0600

    creating tf for wflw- revised

--- main.py
+++ main.py
@@ -29,18 +29,20 @@ if __name__ == '__main__':
     # tf_record_util.rotaate_and_save(dataset_name=DatasetName.cofw)
     # tf_record_util.random_augment_from_rotated(dataset_name=DatasetName.cofw)
     '''     normalize the points and save'''
-    tf_record_util.normalize_points_and_save(dataset_name=DatasetName.wflw)
+    # tf_record_util.normalize_points_and_save(dataset_name=DatasetName.wflw)
     '''     generate pose using hopeNet'''
-    tf_record_util.detect_pose_and_save(dataset_name=DatasetName.wflw)
+    # tf_record_util.detect_pose_and_save(dataset_name=DatasetName.wflw)
 
     '''     create and save PCA objects'''
     # pca_utility.create_pca_from_points(DatasetName.ibug, 95)
-    # pca_utility.create_pca_from_npy(DatasetName.cofw, 95)
+    pca_utility.create_pca_from_npy(DatasetName.wflw, 95)
+    pca_utility.create_pca_from_npy(DatasetName.wflw, 100)
 
     '''     create tfRecord:'''
     # tf_record_util.create_tf_record(dataset_name=DatasetName.ibug, dataset_type=None, heatmap=False, accuracy=100)
     # tf_record_util.create_tf_record(dataset_name=DatasetName.cofw, dataset_type=None, heatmap=False, accuracy=100)
-    # tf_record_util.create_tf_record(dataset_name=DatasetName.wflw, dataset_type=None, heatmap=False, accuracy=100)
+    tf_record_util.create_tf_record(dataset_name=DatasetName.wflw, dataset_type=None, heatmap=False, accuracy=100)
+    tf_record_util.create_tf_record(dataset_name=DatasetName.wflw, dataset_type=None, heatmap=False, accuracy=95)
 
     '''--> retrive and test tfRecords'''
     # tf_record_util.test_tf_record()
