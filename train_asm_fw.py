a7e1ee4e0d1559e0adc877bee3fe0f7d5a2a5e96
aliprf <aliprf@gmail.com>
2020-12-07 10:24:49 -0700

    efnet

--- train_asm_fw.py
+++ train_asm_fw.py
@@ -53,10 +53,10 @@ class Train:
             "./train_logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
 
         '''making models'''
-        _lr = 0.000002
+        _lr = 1e-10
         model = self.make_model(arch=arch, w_path=weight_path)
         '''create optimizer'''
-        optimizer = self._get_optimizer(lr=_lr, beta_1=0.9, beta_2=0.999, decay=1e-6)
+        optimizer = self._get_optimizer(lr=_lr, beta_1=0.5, beta_2=0.999, decay=1e-6)
 
         '''create sample generator'''
         x_train_filenames, x_val_filenames, y_train_filenames, y_val_filenames = self._create_generators()
