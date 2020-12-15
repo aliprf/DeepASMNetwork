5e4703bbbffc2ca789905a996b833dffa9275cdf
aliprf <aliprf@gmail.com>
2020-12-15 10:27:37 -0700

    new asm model

--- train_asm_fw.py
+++ train_asm_fw.py
@@ -52,7 +52,7 @@ class Train:
             "./train_logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
 
         '''making models'''
-        _lr = 1e-1
+        _lr = 9e-1
         model = self.make_model(arch=arch, w_path=weight_path)
         '''create optimizer'''
         optimizer = self._get_optimizer(lr=_lr)
